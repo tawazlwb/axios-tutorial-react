34f1db0b896f134ad3d50d00948a1ae473b3315f
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-21 17:48:27 +0200

    183. Global Default

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,17 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
+import axios from 'axios'
 
-const productsUrl = 'https://course-api.com/react-store-products';
-const randomUserUrl = 'https://randomuser.me/api';
+const productsUrl = 'https://course-api.com/react-store-products'
+const randomUserUrl = 'https://randomuser.me/api'
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
-  };
+    console.log('global axios instance')
+    try {
+      const response1 = await axios(productsUrl)
+      const response2 = await axios(randomUserUrl)
+      console.log('productsUrl :', response1)
+      console.log('randomUserUrl :', response2)
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
-  return <h2 className='text-center'>global instance</h2>;
-};
-export default GlobalInstance;
+  return <h2 className='text-center'>global instance</h2>
+}
+export default GlobalInstance
