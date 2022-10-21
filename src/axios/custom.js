0cd473f5e07e8bfa5c7dfde2706c00cc29562627
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-21 17:59:22 +0200

    184. Custom Instance

---
+++ src/axios/custom.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+export const authFetch = axios.create({
+  baseURL: 'https://course-api.com',
+  headers: {
+    Accept: 'application/json',
+  },
+})
+
+export default authFetch
--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,16 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
+import axios from 'axios'
+import { authFetch } from '../axios/custom'
 
-const randomUserUrl = 'https://randomuser.me/api';
+const randomUserUrl = 'https://randomuser.me/api'
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
-  };
+    console.log('custom axios instance')
+    try {
+      const response1 = await authFetch('/react-store-products')
+      const response2 = await axios(randomUserUrl)
+      console.log('productsUrl :', response1)
+      console.log('randomUserUrl :', response2)
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
-  return <h2 className='text-center'>custom instance</h2>;
-};
-export default CustomInstance;
+  return <h2 className='text-center'>custom instance</h2>
+}
+export default CustomInstance
